739ad572dbf3a46b1a9745c9412303bf5409be0d
Sejoslaw <k.dobrzynski94@gmail.com>
2022-08-10 11:23:23 +0200

    +tests;

---
+++ src/Dova.Examples.Tests/ConsoleTests.cs
@@ -0,0 +1,41 @@
+using Dova.Core;
+using Dova.Core.Runtime;
+using Dova.InterfaceProviders.Moq;
+using Dova.JDK.Extensions;
+using Dova.JDK.java.lang;
+using Xunit;
+
+namespace Dova.Examples.Tests;
+
+public class ConsoleTests
+{
+    [Fact]
+    public void Should_print_on_console_without_using_extensions()
+    {
+        Setup();
+
+        var text = "Hello World from JVM using Dova.JDK";
+        
+        DovaVM.Initialize(new DovaConfiguration());
+
+        var textPtr = DovaVM.Runtime.GetString(text);
+        var str = new String(textPtr);
+        
+        JDK.java.lang.System.out_Property.println(str);
+    }
+    
+    [Fact]
+    public void Should_print_on_console_using_extensions()
+    {
+        Setup();
+
+        DovaVM.Initialize(new DovaConfiguration());
+
+        JDK.java.lang.System.out_Property.println("Hello World from JVM using Dova.JDK".ToJava());
+    }
+
+    private void Setup()
+    {
+        MoqInterfaceObjectProvider.Setup();
+    }
+}
