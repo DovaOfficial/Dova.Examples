508aa458853cd758ab6931a4a67b7061462297b8
Sejoslaw <k.dobrzynski94@gmail.com>
2022-08-11 13:52:21 +0200

    +JFrame test;

---
+++ src/Dova.Examples.Tests/JFrameTests.cs
@@ -0,0 +1,33 @@
+using System.Threading;
+using Dova.Core;
+using Dova.JDK.Extensions;
+using Dova.JDK.java.awt;
+using Dova.JDK.javax.swing;
+using Xunit;
+
+namespace Dova.Examples.Tests;
+
+public class JFrameTests : BaseTestClass
+{
+    [Fact]
+    public void Should_create_simple_window()
+    {
+        Setup();
+
+        DovaVM.Initialize(new DovaConfiguration());
+        
+        var jframe = new JFrame("Simple Window using JNI".ToJava());
+        jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE_Property);
+
+        var jlabel = new JLabel("Sample Label using JNI".ToJava(), SwingConstants.CENTER_Property);
+        jlabel.setPreferredSize(new Dimension(300, 100));
+
+        jframe.getContentPane().add(jlabel, BorderLayout.CENTER_Property);
+        
+        jframe.setLocationRelativeTo(null);
+        jframe.pack();
+        jframe.setVisible(true);
+        
+        Thread.Sleep(5000);
+    }
+}
